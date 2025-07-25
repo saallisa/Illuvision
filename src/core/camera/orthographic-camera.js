295302fac9978320ac11363d6d4a5cbe0c2d0f8e
Lisa Saalfrank <lisa.saalfrank@web.de>
2025-07-25 23:54:52 +0200

    Remap z coordinates into view range

--- src/core/camera/orthographic-camera.js
+++ src/core/camera/orthographic-camera.js
@@ -7,12 +7,36 @@ import { Matrix4 } from '../matrix4.js';
  */
 class OrthographicCamera extends Camera
 {
+    #near = null;
+    #far = null;
+
+    constructor(near = 0, far = 1000)
+    {
+        super();
+
+        if (near > far) {
+            throw new Error(
+                'The far value must be larger than the near value!'
+            );
+        }
+
+        this.#near = near;
+        this.#far = far;
+    }
+
     /**
-     * Returns the projection matrix for the camera.
-     * This is a placeholder method and should be implemented in subclasses.
+     * Returns the projection matrix for the orthographic camera.
      */
-    getProjectionMatrix() {
-        return Matrix4.createScale(1, this.getAspectRatio(), 1);
+    getProjectionMatrix()
+    {
+        const zLength = this.#far - this.#near;
+        const zScale = 1 / zLength;
+        const zDisplacement = - ((this.#far + this.#near) / zLength);
+
+        const translation = Matrix4.createTranslation(0, 0, zDisplacement);
+        const scaling = Matrix4.createScale(1, this.getAspectRatio(), zScale);
+
+        return translation.multiplyOther(scaling);
     }
 }
 
