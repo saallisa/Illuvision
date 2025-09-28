ad603858b920b741ad3003e760c2aafef78d6c5e
Lisa Saalfrank <lisa.saalfrank@web.de>
2025-09-28 01:38:53 +0000

    One-file namespaced import

--- examples/perspective.js
+++ examples/perspective.js
@@ -1,14 +1,5 @@
 
-import { BasicMaterial } from '/src/core/material/basic-material.js';
-import { Box } from '/src/core/geometry/box.js';
-import { Color } from '/src/core/color.js';
-import { Engine } from '/src/engine.js';
-import { Mesh } from '../src/core/mesh.js';
-import { PerspectiveCamera } from '/src/core/camera/perspective-camera.js';
-import { Plane } from '/src/core/geometry/plane.js';
-import { Scene } from '/src/core/scene.js';
-import { SceneNode } from '/src/core/scene-node.js';
-import { Vector3 } from '/src/core/vector3.js';
+import * as VYXEN from '/src/vyxen.js';
 
 /**
  * Render a plane onto the canvas.
@@ -17,60 +8,60 @@ async function main()
 {
     // Configure engine
     const baseUrl = new URL(window.location.href);
-    Engine.setRootPath(baseUrl.origin + '/src/');
+    VYXEN.Engine.setRootPath(baseUrl.origin + '/src/');
 
     // Init engine
-    const engine = new Engine();
+    const engine = new VYXEN.Engine();
     engine.setSizeToWindow();
-    engine.setClearColor(Color.fromHex('87ceeb')); // sky blue
+    engine.setClearColor(VYXEN.Color.fromHex('87ceeb')); // sky blue
     await engine.initialize();
 
     // Add canvas to page
     document.body.appendChild(engine.getCanvas());
 
     // Create a simple ground plane
-    const groundGeometry = new Plane(5, 5, 1, 1);
+    const groundGeometry = new VYXEN.Plane(5, 5, 1, 1);
 
     // Define the material to use
-    const groundMaterial = await BasicMaterial.init({
-        color: Color.GREEN
+    const groundMaterial = await VYXEN.BasicMaterial.init({
+        color: VYXEN.Color.GREEN
     });
 
     // Create a mesh from the geometry and material
-    const ground = new Mesh(groundGeometry, groundMaterial);
+    const ground = new VYXEN.Mesh(groundGeometry, groundMaterial);
 
     // Create a scene node for the ground
-    const sceneNode = new SceneNode(ground);
-    sceneNode.setPosition(new Vector3(0, 0, 0));
+    const sceneNode = new VYXEN.SceneNode(ground);
+    sceneNode.setPosition(new VYXEN.Vector3(0, 0, 0));
 
     // Rotate the scene node so that it covers the ground
     sceneNode.rotateX = 90;
 
     // Create a simple box
-    const boxGeometry = new Box(1, 1, 1);
+    const boxGeometry = new VYXEN.Box(1, 1, 1);
 
     // Define the material to use
-    const boxMaterial = await BasicMaterial.init({
-        color: Color.BLUE
+    const boxMaterial = await VYXEN.BasicMaterial.init({
+        color: VYXEN.Color.BLUE
     });
 
     // Create a mesh from the geometry and material
-    const box = new Mesh(boxGeometry, boxMaterial);
+    const box = new VYXEN.Mesh(boxGeometry, boxMaterial);
 
     // Create a second scene node for the box
-    const sceneNode2 = new SceneNode(box);
-    sceneNode2.setPosition(new Vector3(0, 1, 0));
+    const sceneNode2 = new VYXEN.SceneNode(box);
+    sceneNode2.setPosition(new VYXEN.Vector3(0, 1, 0));
 
     // Create a new Scene and add the node to it
-    const scene = new Scene();
+    const scene = new VYXEN.Scene();
     scene.addNode(sceneNode);
     scene.addNode(sceneNode2);
 
     // Create an perspective camera
-    const camera = new PerspectiveCamera(45, 0.1, 100);
+    const camera = new VYXEN.PerspectiveCamera(45, 0.1, 100);
     camera.setAspectRatio(engine.getAspectRatio());
-    camera.setTarget(new Vector3(0, 0, 0));
-    camera.setPosition(new Vector3(5, 5, 5));
+    camera.setTarget(new VYXEN.Vector3(0, 0, 0));
+    camera.setPosition(new VYXEN.Vector3(5, 5, 5));
 
     // Create the function for the animation loop 
     const animation = function ()
---
+++ src/vyxen.js
@@ -0,0 +1,52 @@
+
+import { BasicMaterial } from './core/material/basic-material.js';
+import { Box } from './core/geometry/box.js';
+import { Color } from './core/color.js';
+import { Engine } from './engine.js';
+import { Face } from './core/face.js';
+import { Geometry } from './core/geometry/geometry.js';
+import { Mesh } from './core/mesh.js';
+import { OrthographicCamera } from './core/camera/orthographic-camera.js';
+import { PerspectiveCamera } from './core/camera/perspective-camera.js';
+import { Plane } from './core/geometry/plane.js';
+import { Scene } from './core/scene.js';
+import { SceneNode } from './core/scene-node.js';
+import { Triangle } from './core/geometry/triangle.js';
+import { Uv } from './core/uv.js';
+import { Vector3 } from './core/vector3.js';
+
+export {
+    BasicMaterial,
+    Box,
+    Color,
+    Engine,
+    Face,
+    Geometry,
+    Mesh,
+    OrthographicCamera,
+    PerspectiveCamera,
+    Plane,
+    Scene,
+    SceneNode,
+    Triangle,
+    Uv,
+    Vector3
+}
+
+export default {
+    BasicMaterial,
+    Box,
+    Color,
+    Engine,
+    Face,
+    Geometry,
+    Mesh,
+    OrthographicCamera,
+    PerspectiveCamera,
+    Plane,
+    Scene,
+    SceneNode,
+    Triangle,
+    Uv,
+    Vector3
+}
