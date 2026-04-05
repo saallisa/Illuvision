f63d1ceb491f8cdb58642327139a85cd1ed7bbd0
Lisa Saalfrank <lisa.saalfrank@web.de>
2026-04-05 12:03:49 +0200

    Add raw texture mode to lambert materials

--- src/core/material/lambert-material.js
+++ src/core/material/lambert-material.js
@@ -47,11 +47,12 @@ class LambertMaterial extends Material
         const validModes = [
             Material.VERTEX_COLOR,
             Material.UNIFORM_COLOR,
-            Material.COLOR_BLEND
+            Material.COLOR_BLEND,
+            Material.TEXTURE_RAW
         ];
 
         if (!validModes.includes(mode)) {
-            throw new Error(`Invalid color: ${mode}.`);
+            throw new Error(`Invalid color mode: ${mode}.`);
         }
     }
 }
